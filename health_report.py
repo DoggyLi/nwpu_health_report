1c38b3bd176532127a9b6d1a1d00c1ad16255e5e
DoggyLi <62590752+DoggyLi@users.noreply.github.com>
2022-10-09 13:55:32 +0800

    Update health_report.py

--- health_report.py
+++ health_report.py
@@ -12,8 +12,9 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-browser = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
 time.sleep(1)
+browser = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
+
 
 # open nwpu
 browser.get('https://uis.nwpu.edu.cn/cas/login?service=https://ecampus.nwpu.edu.cn/')
