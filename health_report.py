b854003e1fbad9dcb419cfba10351a2b8f915b17
DoggyLi <62590752+DoggyLi@users.noreply.github.com>
2022-10-10 12:44:16 +0800

    Update health_report.py

--- health_report.py
+++ health_report.py
@@ -29,9 +29,9 @@ password.send_keys(PASSWORD)
 browser.find_element(By.NAME, "button").click()
 time.sleep(1)
 
-browser.get('https://yqtb.nwpu.edu.cn/wx/ry/jrsb_xs.jsp')
-browser.refresh()
-time.sleep(1)
+# browser.get('https://yqtb.nwpu.edu.cn/wx/ry/jrsb_xs.jsp')
+# browser.refresh()
+# time.sleep(1)
 print(browser.title)
 
 
